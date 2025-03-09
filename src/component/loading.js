f0cf0b2f83c5291a72144ae70d1108a1c5b49dc3
viyom shukla <viyomshukla21@gmail.com>
2025-03-09 19:13:33 +0530

    second commit

--- src/component/loading.js
+++ src/component/loading.js
@@ -3,39 +3,41 @@ import { useNavigate } from 'react-router-dom';
 import './loading.css';
 
 const LandingPage = () => {
-  const [animationStarted, setAnimationStarted] = useState(false);
-  const [showNewImage, setShowNewImage] = useState(false);
-  const [showNewElements, setShowNewElements] = useState(false);
+  const [aniStart, setaniStart] = useState(false);
+  const [showellipse, setshowellipse] = useState(false);
+  const [showsemi, setshowsemi] = useState(false);
   const [grad, setGrad] = useState(false);
   const navigate = useNavigate();
 
   useEffect(() => {
-    setAnimationStarted(true);
+    setaniStart(true);
 
-    // Show first new image
+   
     const timer1 = setTimeout(() => {
-      setShowNewImage(true);
+    
+
+      setshowellipse(true);
     }, 10000);
 
-    // Hide first image after 1 second
+   
     const timer2 = setTimeout(() => {
-      setShowNewImage(false);
+      setshowellipse(false);
     }, 11000);
 
-    // Show multiple elements exactly 1 second after timer2 ends
+   
     const timer3 = setTimeout(() => {
-      setShowNewElements(true);
+      setshowsemi(true);
     }, 11500);
 
-    // Show GradHub after timer3
+
     const timer4 = setTimeout(() => {
       setGrad(true);
-    }, 12500); // Starts 1 second after timer3
+    }, 12500); 
 
-    // Redirect to main.js after showing GradHub
+  
     const timer5 = setTimeout(() => {
       navigate('/main');
-    }, 14000); // Starts 1.5 seconds after timer4
+    }, 14000); 
 
     return () => {
       clearTimeout(timer1);
@@ -43,20 +45,20 @@ const LandingPage = () => {
       clearTimeout(timer3);
       clearTimeout(timer4);
       clearTimeout(timer5);
-      setAnimationStarted(false);
+      setaniStart(false);
     };
   }, [navigate]);
 
   return (
-    <div className="landing-page">
+    <div className="lp">
       <div className="logo"></div>
-      <div className={`ellipse ${animationStarted ? 'animate' : ''}`}></div>
-      {showNewImage && <div className="new-image"></div>}
-      {showNewElements && (
+      <div className={`ellipse ${aniStart ? 'animate' : ''}`}></div>
+      {showellipse && <div className="new-image"></div>}
+      {showsemi && (
         <>
-          <div className="new-element new-element-1"></div>
-          <div className="new-element new-element-2"></div>
-          <div className="new-element new-element-3"></div>
+          <div className="ne ne-1"></div>
+          <div className="ne ne-2"></div>
+          <div className="ne ne-3"></div>
         </>
       )}
       {grad && <div className="grad"><span>Grad</span>Hub</div>}
